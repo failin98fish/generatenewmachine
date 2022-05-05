4271369a40c07852f79989e558c7484990f556ba
failin98fish <56397393+failin98fish@users.noreply.github.com>
2022-05-06 01:25:28 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir /root/portainer
 echo "finish mkdir"
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /root/portainer:/data portainer/portainer
 echo "done"
