c7ce4e3752ea94d96f202ea318cd0d2475ca0fb4
failin98fish <56397393+failin98fish@users.noreply.github.com>
2022-05-06 01:54:35 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /root/portainer
-cp /root/generatemachine/portainer.yml /root/portainer/docker-compose.yml
+cp /root/generatenewmachine/portainer.yml /root/portainer/docker-compose.yml
 echo "copy success"
 cd /root/portainer
 docker-compose up -d
