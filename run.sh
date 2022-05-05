315a3220c7ec98c33091753601969a2682e7ebb0
failin98fish <56397393+failin98fish@users.noreply.github.com>
2022-05-06 01:24:31 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 mkdir /root/portainer
+echo "finish mkdir"
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /root/portainer:/data portainer/portainer
+echo "done"
 
-
-cd shell_tut
-
-for ((i=0; i<10; i++)); do
-    touch test_$i.txt
-done
